5e7443205b2e80e82b2cfcdccb7ccf237cb3a6cc
Christian Lowe <christglowe@gmail.com>
2018-05-09 14:56:23 -0500

    trying to programmatically add ports of call to map

--- components/voyage.js
+++ components/voyage.js
@@ -6,24 +6,34 @@ import H1 from './h1.js';
 import { MapView } from 'expo';
 const { Marker } = MapView
 
+// {key: '5', coordinate: {latitude: 9.9312, longitude: 76.2673}, title: 'port of Kochi'}
 
 export default class Voyage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { voyageData : ' ',
-                   markers: [{key: '5', coordinate: {latitude: 9.9312, longitude: 76.2673}, title: 'port of Kochi'}]
+                   markers: []
     }
   }
 
   componentWillMount() {
-    fetch('http://semesteratsea-api.herokuapp.com/api/voyages/10', {
+    fetch('http://semesteratsea-api.herokuapp.com/api/voyages/spring&2016', {
       method: 'GET'
     }).then( response => {
        return response.json()
     }).then( (data) => {
+      let i = 0
       this.setState({voyageData: data.data})
-      console.log(data.data);
+      data.data.forEach( (object) => {
+        if (i > 0) {
+          console.log(this.state.markers[5])
+        }
+        this.setState( prevState => {
+          prevState.markers.push({key: i, coordinate: {latitude: Number(object.latitude) ,longitude: Number(object.longitude)}, title: `port of ${object.city}`});
+        })
+        i ++;
+      })
     }).catch(err => {
       console.log(err, 'err')
     })
@@ -41,20 +51,27 @@ export default class Voyage extends React.Component {
       </View>
 
       <View style={{flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
-      <H1 content={`${capitalizeFirstLetter(this.state.voyageData.semester)} ${this.state.voyageData.year}: \n ${this.state.voyageData.tagline}`} />
-      <Image source={{uri: `http://semesteratsea-api.herokuapp.com/${this.state.voyageData.header_photo_url}`}} style={{width: 300, height: 150, alignSelf: 'center'}} />
+      <H1 content={`${capitalizeFirstLetter(this.state.voyageData[0].semester)} ${this.state.voyageData[0].year}: \n ${this.state.voyageData[0].tagline}`} />
+      <Image source={{uri: `http://semesteratsea-api.herokuapp.com/${this.state.voyageData[0].header_photo_url}`}} style={{width: 300, height: 150, alignSelf: 'center'}} />
       <Text> If you ever speak to an SAS alum about what they loved most about the experience, they’ll all tell you it’s the community. Spend your semester making lifelong friendships and sharing a world of experiences together </Text>
       <MapView
        style={{ width: 300, height: 300 }}
-       >
+       initialRegion={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 120.0022,
+         longitudeDelta: 90.0421,
+       }}>
 
-       {this.state.markers.map(marker => (
+       {this.state.markers.map(marker => {
+         console.log(marker)
+         return(
          <Marker
          key = {marker.key}
          coordinate={marker.coordinate}
          title={marker.title}
          />
-       ))}
+       )})}
 
      </MapView >
       </View>
